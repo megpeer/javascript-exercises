48a1d0ef0718556563c7bda7dfa13a056a37d53f
Meg Peer <megimu@gmail.com>
2024-05-01 09:09:31 -0400

    complete javascript exersise 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    else {
+    let string = "";
+        for (i = 0; i < times; i ++) {
+            string += word;
+        }
+    return string;
+    }
 };
+const number = Math.floor(Math.random() * 1000)
+repeatString("hey", 3)
+repeatString('hello', 10)
+repeatString('hi', 1)
+repeatString('bye', 0)
+repeatString('goodbye', -1)
+repeatString('odin', number)
+repeatString('', 10)
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+function reverseString(str) {
+return str.split("").reverse().join("")
 };
 
+reverseString("Hello")
 // Do not edit below this line
 module.exports = reverseString;
