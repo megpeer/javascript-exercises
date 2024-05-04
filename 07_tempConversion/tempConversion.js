a531400a3aeb2aafd4a3ff36ca187226baa59f2f
Meg Peer <megimu@gmail.com>
2024-05-04 10:23:58 -0400

    complete JS exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let cel = ((far - 32) / 1.8)
+  return Math.round(cel * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celc) {
+  let farh = ((celc * 1.8) + 32)
+  return Math.round(farh * 10) / 10
 };
 
 // Do not edit below this line
