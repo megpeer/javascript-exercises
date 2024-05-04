a89c39ba53b387c571fa9715376fafb925ef9c55
Meg Peer <megimu@gmail.com>
2024-05-04 10:04:31 -0400

    complete JS exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0))
 };
+console.log(leapYears(1984))
+console.log(leapYears(1985))
+
+console.log(leapYears(1800))
+console.log(leapYears(1600))
 
 // Do not edit below this line
 module.exports = leapYears;
